246b934a543cea6ccc438325e4c36e41f7cf883c
Steve Pedersen <110639481+steve-pedersen@users.noreply.github.com>
2022-11-01 15:59:08 -0700

    resolved linter errors

--- src/lambdas/index.ts
+++ src/lambdas/index.ts
@@ -1,4 +1,4 @@
-import { Event, Context } from "~/lib";
+import { Event, Context } from '../lib/types';
 
 const CORS_KEY = 'access-control-allow-origin';
 const CONTENT_TYPE = 'content-type';
@@ -14,15 +14,9 @@ const local = process.env.IS_LOCAL ? 'local' : 'cloud';
 
 export const handler = async (event: Event, context: Context) => {
   const { awsRequestId } = context;
-  const {
-    httpMethod,
-    path,
-    queryStringParameters,
-    body,
-    headers
-  } = event;
+  const { httpMethod, path, queryStringParameters, body, headers } = event;
   const correlationId = headers[CORRELATION_ID];
-  
+
   try {
     console.log(`*** event received[${local}] ***`, { url: path, correlationId, awsRequestId });
 
@@ -31,17 +25,15 @@ export const handler = async (event: Event, context: Context) => {
     responseHeaders[CORRELATION_ID] = correlationId || '';
 
     return { headers: responseHeaders, body: JSON.stringify(result), statusCode };
-  } 
-  catch (err) {
+  } catch (err) {
     const { message } = err as Error;
     console.log(`Error: ${message}`, { correlationId, err });
 
     return {
       statusCode: 500,
-      body: message || 'System Error'
-    }
-  }
-  finally {
+      body: message || 'System Error',
+    };
+  } finally {
     console.log('*** event handled ***', { url: path, correlationId });
   }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,46 @@
-const webpack = require('webpack')
-const slsw = require('serverless-webpack')
-const path = require('path')
-const nodeExternals = require('webpack-node-externals')
+const webpack = require('webpack');
+const slsw = require('serverless-webpack');
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = (async () => {
-  const accountId = await slsw.lib.serverless.providers.aws.getAccountId()
+  const accountId = await slsw.lib.serverless.providers.aws.getAccountId();
   return {
     target: 'node',
     entry: slsw.lib.entries,
     externals: [
       nodeExternals({
-        whitelist: [/^core-js/, /^regenerator-runtime/]
-      })
+        whitelist: [/^core-js/, /^regenerator-runtime/],
+      }),
     ],
     optimization: {
-      nodeEnv: false
+      nodeEnv: false,
     },
     mode: 'production',
     plugins: [
       new webpack.DefinePlugin({
-        AWS_ACCOUNT_ID: `${accountId}`
-      })
+        AWS_ACCOUNT_ID: `${accountId}`,
+      }),
     ],
     module: {
       rules: [
         {
           test: /\.js$/,
           exclude: /(node_modules|dependency_layer)/,
-          loader: 'babel-loader'
+          loader: 'babel-loader',
         },
         {
           test: /\.tsx?$/,
           use: 'ts-loader',
-          exclude: /(node_modules|dependency_layer)/
-        }
-      ]
+          exclude: /(node_modules|dependency_layer)/,
+        },
+      ],
     },
     resolve: {
       alias: {
-        '~': path.resolve(__dirname, 'src')
+        '~': path.resolve(__dirname, 'src'),
       },
-      extensions: ['.ts', '.js']
-    }
-  }
-})()
+      extensions: ['.ts', '.js'],
+    },
+  };
+})();
